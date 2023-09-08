e1d66edb98dc3a21c15af55f4496f988d944e634
Nagesh <nageshlucky3046@gmail.com>
2023-09-08 13:02:57 +0530

    Practiced waits

---
+++ sept9/test_commands.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+def test_pycommands():
+    driver=webdriver.Chrome()
+    mywait=WebDriverWait(driver,10)
+    driver.maximize_window()
+    driver.get("https://demo.nopcommerce.com/")
+    register=mywait.until(EC.presence_of_element_located((By.CSS_SELECTOR,".ico-register")))
+    register.click()
